c54c650e563b68683190800510ef078d0711a2da
mindflix <nyco.demol@gmail.com>
2021-11-16 18:18:48 +0100

    up

--- project/task/__init__.py
+++ project/task/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, g
+from flask import Blueprint, render_template, request, flash, redirect, url_for, g, abort
 from flask import current_app as app
 from project.db import get_db
 from project.auth import login_required
@@ -43,3 +43,59 @@ def create():
             return redirect(url_for('task'))
 
     return render_template('task/create.html')
+
+
+@task_bp.route('/task/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+
+    task = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE task SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('task'))
+
+    return render_template('task/update.html', task=task)
+
+
+def get_post(id, check_author=True):
+    task = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM task p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if task is None:
+        abort(404, f"Task id {id} doesn't exist.")
+
+    if check_author and task['author_id'] != g.user['id']:
+        abort(403)
+
+    return task
+
+
+@task_bp.route('/task/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM task WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('task'))
